2195d2039bca1a1882414e1edd948fc9668c70bf
Tony Tkacik <tonydamage@gmail.com>
2014-01-29 16:52:35 +0000

    Added Ubuntu Tweak tool, removed some scopes, added CPU indicator.
    Signed-off-by: Tony Tkacik <tonydamage@gmail.com>

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -1,1 +1,17 @@
 #!/bin/sh
+
+
+echo Removing Unity Scopes
+
+sudo apt-get  remove unity-scope-musicstores unity-scope-openclipart unity-scope-yelp
+
+echo Installing Unity Tweak Tool
+
+sudo add-apt-repository ppa:freyja-dev/unity-tweak-tool-daily
+sudo apt-get update 
+sudo apt-get install unity-tweak-tool
+
+
+echo Installing Indicators
+
+sudo apt-get install indicator-multiload
