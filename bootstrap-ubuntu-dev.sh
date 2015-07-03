6bed7a7bd844b31f4e1e395d9f1878c5ebdcc559
Tony Tkacik <tonydamage@gmail.com>
2015-07-03 12:02:51 +0200

    Added global gitignore.
    Signed-off-by: Tony Tkacik <tonydamage@gmail.com>

--- bootstrap-ubuntu-dev.sh
+++ bootstrap-ubuntu-dev.sh
@@ -5,3 +5,5 @@ echo "Installing Oracle Java."
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
+
+git config --global core.excludesfile ~/Environment/gitignore-global
