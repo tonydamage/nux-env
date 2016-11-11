9aa2386987cc64070fbd7a557b288233e7b4d69b
Tony Tkacik <tonydamage@gmail.com>
2016-11-11 17:18:54 +0100

    Splitted part of nux-ranner into include script.
    Signed-off-by: Tony Tkacik <tonydamage@gmail.com>

---
+++ inc/nux-base.sh
@@ -0,0 +1,23 @@
+
+readonly NC_No='\033[0m' # No Color
+readonly NC_Black='\033[0;30m'
+readonly NC_Green='\033[0;32m'
+readonly NC_Red='\033[0;31m'
+readonly NC_BrownOrange='\033[0;33m'
+readonly NC_Blue='\033[0;34m'
+readonly NC_Purple='\033[0;35m'
+readonly NC_Cyan='\033[0;36m'
+readonly NC_LightGray='\033[0;37m'
+
+readonly NC_DarkGray='\033[1;30m'
+readonly NC_LightRed='\033[1;31m'
+readonly NC_LightGreen='\033[1;32m'
+readonly NC_Yellow='\033[1;33m'
+readonly NC_LightBlue='\033[1;34m'
+readonly NC_LightPurple='\033[1;35m'
+readonly NC_LightCyan='\033[1;36m'
+readonly NC_White='\033[1;37m'
+
+NC_Debug=$NC_LightGray
+NC_Error=$NC_LightRed
+NC_Warning=$NC_Yellow
