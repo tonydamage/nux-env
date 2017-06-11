089cb5ccce5ca1958cdce9a724e2f47e860c3f5f
Tony Tkacik <tonydamage@gmail.com>
2017-06-11 17:23:55 +0200

    shelldoc: Added micro markup support for shell comments.
    Added support for micro markup to ease reading and to
    highlight keywords and headings in documentation derived
    from shell comments.
    
    nuxfs and nux-runner takes advantage from this.
    
    Signed-off-by: Tony Tkacik <tonydamage@gmail.com>

--- inc/nux-base.inc.sh
+++ inc/nux-base.inc.sh
@@ -2,12 +2,33 @@
 readonly NUX_INC_DIR=$(dirname $(realpath  ${BASH_SOURCE[0]}))
 readonly NUX_ENV_DIR=$(dirname $NUX_INC_DIR)
 
-# Color defintions
+# Color definitions
 
-readonly NC_No='\033[0m' # No Color
+readonly nc_bold=`tput setaf 0`
+readonly nc_bg_bold=`tput setab 0`
+readonly nc_black=`tput setab 0`
+readonly nc_bg_black=`tput setab 0`
+readonly nc_cyan=`tput setaf 6`
+readonly nc_bg_cyan=`tput setab 6`
+readonly nc_magenta=`tput setaf 5`
+readonly nc_bg_magenta=`tput setab 5`
+readonly nc_red=`tput setaf 1`
+readonly nc_bg_red=`tput setab 1`
+readonly nc_white=`tput setaf 7`
+readonly nc_bg_white=`tput setab 7`
+readonly nc_green=`tput setaf 2`
+readonly nc_bg_green=`tput setab 2`
+readonly nc_yellow=`tput setaf 3`
+readonly nc_bg_yellow=`tput setab 3`
+readonly nc_blue=`tput setaf 4`
+readonly nc_bg_blue=`tput setab 4`
+readonly nc_end=`tput sgr0`
+
+readonly NC_Bold=`tput bold`
+readonly NC_No=`tput sgr0` # No Color
 readonly NC_Black='\033[0;30m'
 readonly NC_Green='\033[0;32m'
-readonly NC_Red='\033[0;31m'
+readonly NC_Red=$nc_bold$nc_red
 readonly NC_BrownOrange='\033[0;33m'
 readonly NC_Blue='\033[0;34m'
 readonly NC_Purple='\033[0;35m'
@@ -16,11 +37,11 @@ readonly NC_LightGray='\033[0;37m'
 readonly NC_DarkGray='\033[1;30m'
 readonly NC_LightRed='\033[1;31m'
 readonly NC_LightGreen='\033[1;32m'
-readonly NC_Yellow='\033[1;33m'
+readonly NC_Yellow=$nc_yellow
 readonly NC_LightBlue='\033[1;34m'
 readonly NC_LightPurple='\033[1;35m'
 readonly NC_LightCyan='\033[1;36m'
-readonly NC_White='\033[1;37m'
+readonly NC_White=$nc_white
 
 # Color for message levels
 NC_info=$NC_LightGray
@@ -85,4 +106,23 @@ function nux.exec.optional {
 
 function nux.dirty.urlencode {
     echo -n "$1" | sed "s/ /%20/g"
+}
+
+function nux.help.comment {
+  local source="$1"
+  grep -E "^\#\#( |$)" "$source" \
+    | cut -d\# -f3- \
+    | cut -d" " -f2- \
+    | nux.help.shelldoc
+
+}
+
+function nux.help.shelldoc {
+  cat | sed -r \
+    -e "s/^## ?(.*)/${NC_White}\1${NC_No}/gI" \
+    -e "s/^# ?(.*)/${NC_Bold}\1${NC_No}/gI" \
+    -e "s/^   ?[a-z0-9.-_]*/${NC_Bold}&${NC_No}/gI" \
+    -e "s/\*\*([^*]*)\*\*/${NC_Bold}\1${NC_No}/gI"  \
+    -e "s/\*([^*]*)\*/${NC_White}\1${NC_No}/gI"  \
+
 }
